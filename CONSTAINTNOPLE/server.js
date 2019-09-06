b591e4f00c80a4b9b72168ced85567a811b89ff7
Mekhib <Mekhi19132@gmail.com>
2019-09-05 18:19:34 -0700

    this version can be deployed to heroku and has the server running with 2 HTML routes

--- CONSTAINTNOPLE/server.js
+++ CONSTAINTNOPLE/server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4500;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -14,34 +14,34 @@ app.use(express.static("public"));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// // Routes
+// require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    app.listen(PORT, function() {
+        console.log(
+            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            PORT,
+            PORT
+        );
+    });
 });
 
 module.exports = app;
