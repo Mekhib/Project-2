199ded3372725b7d12df3579ab5399ff372d9cb6
Mekhib <Mekhi19132@gmail.com>
2019-09-10 18:23:49 -0700

    added index.js

--- CONSTAINTNOPLE/models/friends.js
+++
@@ -1,7 +0,0 @@
-module.exports = function table(sequelize, DataTypes) {
-    var friends = sequelize.define("friends", {
-        friend1: DataTypes.INTEGER,
-        friend2: DataTypes.INTEGER,
-    })
-    return friends
-}
--- CONSTAINTNOPLE/models/request.js
+++
@@ -1,7 +0,0 @@
-module.exports = function request(sequelize, DataTypes) {
-    var request = sequelize.define("request", {
-        requester: DataTypes.INTEGER,
-        recipient: DataTypes.INTEGER,
-    })
-    return request
-}
