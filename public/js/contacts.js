5c31e9a99b4cc504f1ba5fd9452ce885e6153eb6
sigismondi <1.1.1.1sigismondi@gmail.com>
2019-09-14 10:41:41 -0400

    added js for search box

---
+++ public/js/contacts.js
@@ -0,0 +1,20 @@
+$("#searchBtn").on("click", (function(event) {
+    event.preventDefault();
+    console.log("you clicked me!")
+       var email = $("#search").val()
+    console.log(email)
+    $.get("/searchresults/" + email, function(data) {
+        console.log(data)
+    $("#newContact").empty();
+    for (var i = 0; i < data.length; i++) {
+    console.log(data[i])
+    if (!data.id === 0) {
+    $("#newContact").append("Nobody exists with that email, try again :)");
+    }
+    else {
+    $("#newContact").append(data[i].first_name);
+    $("#newContact").append("<h2>" + data[i].email + "</h2>");
+ }
+ }
+ })
+ }))
